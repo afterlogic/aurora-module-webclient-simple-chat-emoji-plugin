cbd30002d98704d4327d42cea37993f531ba7165
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:29:19 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -1,30 +1,25 @@
 'use strict';
 
 module.exports = function (oAppData) {
-	return {
-		/**
-		 * Returns true if simple chat emoji module is available for certain user role and public or not public mode.
-		 * 
-		 * @param {int} iUserRole User role, wich enum values are described in %PathToCoreWebclientModule%/js/enums.js
-		 * @param {boolean} bPublic **true** if applications runs in public mode (for example, public calendar or public contact)
-		 * 
-		 * @returns {Boolean}
-		 */
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.NormalUser || iUserRole === Enums.UserRole.Customer;
-		},
-		
-		/**
-		 * Runs before application start. Subscribes to the event before post displaying.
-		 * 
-		 * @param {Object} ModulesManager
-		 */
-		start: function (ModulesManager) {
-			var App = require('%PathToCoreWebclientModule%/js/App.js');
-			
-			App.subscribeEvent('SimpleChat::DisplayPost::before', function (oParameters) {
-				oParameters.Post.displayText = oParameters.Post.text.replace(':)', '&#128522;');
-			});
-		}
-	};
+	var App = require('%PathToCoreWebclientModule%/js/App.js');
+	
+	if (App.getUserRole() === Enums.UserRole.NormalUser || App.getUserRole() === Enums.UserRole.Customer)
+	{
+		return {
+			/**
+			 * Runs before application start. Subscribes to the event before post displaying.
+			 * 
+			 * @param {Object} ModulesManager
+			 */
+			start: function (ModulesManager) {
+				var App = require('%PathToCoreWebclientModule%/js/App.js');
+
+				App.subscribeEvent('SimpleChat::DisplayPost::before', function (oParameters) {
+					oParameters.Post.displayText = oParameters.Post.text.replace(':)', '&#128522;');
+				});
+			}
+		};
+	}
+	
+	return null;
 };
